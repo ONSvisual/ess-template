1ee35535f54b01dc84735fa778bd4fb0239bd477
Andrew Hillman <ajhillman_ddj@outlook.com>
2023-11-17 10:52:52 +0000

    Updates

---
+++ _app/immutable/chunks/stores.b23c4f36.js
@@ -0,0 +1,2 @@
+//js
+import"./index.2e3955ad.js";import{d as e}from"./singletons.a2385025.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
