8a9e875adc73a2992723e91390f5bb14fdcb2d3b
Andrew Hillman <ajhillman_ddj@outlook.com>
2023-11-17 10:47:11 +0000

    Updates

---
+++ _app/immutable/nodes/2.c215cb28.js
@@ -0,0 +1,2 @@
+//js
+import{P as r}from"../chunks/public.73b3fb0a.js";import"../chunks/paths.3ccfdab1.js";import"../chunks/index.34b8e95a.js";import"../chunks/config.ec2e1162.js";import"../chunks/index.2e3955ad.js";import{L as d}from"../chunks/layout.426df739.js";const t=r,e="always";async function a({fetch:n,params:o}){return{areacd:o.code}}const m=Object.freeze(Object.defineProperty({__proto__:null,load:a,prerender:t,trailingSlash:e},Symbol.toStringTag,{value:"Module"}));export{d as component,m as universal};
